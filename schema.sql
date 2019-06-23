78869c0fdc3ead0c0469af710579d9ad246c01d8
Kindeep <kk17xg@brocku.ca>
2019-06-22 23:42:35 -0400

    Relations added

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 /**
  * SQL code to create the database.  
  */
+/* Entities */
 CREATE TABLE Skill (
     skill_id INTEGER PRIMARY KEY,
     min_level INTEGER,
@@ -84,5 +85,118 @@ CREATE TABLE Main_Weapon (
 CREATE TABLE Secondary_Equipment (
     eqp_id INTEGER PRIMARY KEY,
     FOREIGN KEY (eqp_id) REFERENCES Equipment ON DELETE CASCADE
+);
+
+/* Relationships */
+CREATE TABLE Earned_Skill (
+    skill_id INTEGER,
+    cls_name CHAR(20),
+    PRIMARY KEY (skill_id, cls_name),
+    FOREIGN KEY skill_id REFERENCES Skill,
+    FOREIGN KEY cls_name REFERENCES Class
+);
+
+CREATE TABLE Auto_Skill (
+    skill_id INTEGER,
+    cls_name CHAR(20),
+    PRIMARY KEY (skill_id, cls_name),
+    FOREIGN KEY skill_id REFERENCES Skill,
+    FOREIGN KEY cls_name REFERENCES Class
+);
+
+CREATE TABLE Has_Earned (
+    skill_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (skill_id, chr_name),
+    FOREIGN KEY skill_id REFERENCES Skill,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Class_Equipment (
+    cls_name char(20),
+    eqp_id INTEGER,
+    PRIMARY KEY (cls_name, eqp_id),
+    FOREIGN KEY cls_name REFERENCES Class,
+    FOREIGN KEY eqp_id REFERENCES Equipment
+);
+
+CREATE TABLE Embedded_Gems (
+    gem_id INTEGER,
+    eqp_id INTEGER,
+    PRIMARY KEY (gem_id, eqp_id),
+    FOREIGN KEY gem_id REFERENCES Gem,
+    FOREIGN KEY eqp_id REFERENCES Equipment
+);
+
+CREATE TABLE Has_Class (
+    chr_name CHAR(30) NOT NULL,
+    cls_name CHAR(20),
+    PRIMARY KEY (chr_name, cls_name),
+    FOREIGN KEY chr_name REFERENCES Charachter,
+    FOREIGN KEY cls_name REFERENCES Class
+);
+
+CREATE TABLE Clan_Member (
+    cln_name CHAR(30) NOT NULL,
+    chr_name CHAR(30) NOT NULL,
+    PRIMARY KEY (cln_name, chr_name),
+    FOREIGN KEY cln_name REFERENCES Clan,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Clan_Chief (
+    cln_name CHAR(30) NOT NULL,
+    chr_name CHAR(30),
+    PRIMARY KEY (cln_name, chr_name),
+    FOREIGN KEY cln_name REFERENCES Clan,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Head_Equipped (
+    eqp_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (eqp_id, chr_name),
+    FOREIGN KEY eqp_id REFERENCES Equipment,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Chest_Equipped (
+    eqp_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (eqp_id, chr_name),
+    FOREIGN KEY eqp_id REFERENCES Equipment,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Legs_Equipped (
+    eqp_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (eqp_id, chr_name),
+    FOREIGN KEY eqp_id REFERENCES Equipment,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Feet_Equipped (
+    eqp_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (eqp_id, chr_name),
+    FOREIGN KEY eqp_id REFERENCES Equipment,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Main_Equipped (
+    eqp_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (eqp_id, chr_name),
+    FOREIGN KEY eqp_id REFERENCES Equipment,
+    FOREIGN KEY chr_name REFERENCES Charachter
+);
+
+CREATE TABLE Secondary_Equipped (
+    eqp_id INTEGER,
+    chr_name CHAR(30),
+    PRIMARY KEY (eqp_id, chr_name),
+    FOREIGN KEY eqp_id REFERENCES Equipment,
+    FOREIGN KEY chr_name REFERENCES Charachter
 );
 
