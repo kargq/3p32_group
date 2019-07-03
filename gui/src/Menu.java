bdf91d26997d0ed3781f9bcd66436ddd3ea650a0
peakyDicers <ian.chui@hotmail.com>
2019-07-03 13:12:34 -0400

    began gui.

---
+++ gui/src/Menu.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Menu {
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JSpinner spinner1;
+    private JButton btn_addSkill;
+    private JTextField textField2;
+    private JButton assignSkillButton;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+}
