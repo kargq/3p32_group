c7b80771fa9441b6ff0acda03af404fffd393078
Kindeep <kk17xg@brocku.ca>
2019-07-03 14:47:00 -0400

    chaned armour relations to one realation, removed embedded gems table, triggers outline.

---
+++ triggers.sql
@@ -0,0 +1,31 @@
+/* 
+change level after each gain of 1000 experience
+change stats according to level change
+
+initialization values to 10 for all somehow too?
+
++------------+------+-------+----------+---------+------+-------+
+|            | Life | Power | Strength | Defence | Will | Speed |
++------------+------+-------+----------+---------+------+-------+
+| Warrior    | 50   | 2     | 5        | 5       | 1    | 3     |
++------------+------+-------+----------+---------+------+-------+
+| Ranger     | 30   | 2     | 4        | 3       | 3    | 5     |
++------------+------+-------+----------+---------+------+-------+
+| White mage | 15   | 5     | 1        | 2       | 5    | 2     |
++------------+------+-------+----------+---------+------+-------+
+| Black mage | 20   | 5     | 1        | 2       | 5    | 2     |
++------------+------+-------+----------+---------+------+-------+
+*/
+
+
+/* 
+min level for skill (equal or lower than charachter's level)
+*/
+
+/*
+min level for equipment
+/*
+
+/*
+gem level, max gems
+*/
