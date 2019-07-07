f2f24412ba24a0895fe75f564a6ef75b37bc758f
Ian Chui <ian.chui@hotmail.com>
2019-07-06 22:55:20 -0400

    added jar.

---
+++ src/main/GenerateDummyData.java
@@ -0,0 +1,12 @@
+package main;
+
+public class GenerateDummyData {
+    /**
+     * This file is used for generating dummy data.
+     * You are likely looking for the other main method, in the Main.java file.
+     *
+     */
+    public static void main(String[] args) {
+        Toybox.generateMassiveMockData();
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,16 +10,10 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame f = new JFrame();
-        f.setContentPane(new Menu().getPanel());
-        f.setVisible(true);
-        f.setSize(500, 500);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        /* Uncomment this to generate all dummy data.
-         Remember to comment it back after running. */
-
-        //Toybox.generateMassiveMockData();
+//        JFrame f = new JFrame();
+//        f.setContentPane(new Menu().getPanel());
+//        f.setVisible(true);
+//        f.setSize(500, 500);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
