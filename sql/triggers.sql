920b51adeed622629ca1ab751031e6cf6170df2c
Kindeep <kk17xg@brocku.ca>
2019-07-06 20:06:03 -0400

    added trigger for skill assignment check

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,3 +1,6 @@
+-- following command only needs to be run once per database
+create language plpgsql;
+
 /* 
 change level after each gain of 1000 experience
 change stats according to level change
@@ -20,9 +23,26 @@ initialization values to 10 for all somehow too?
 
 
 -- min level for skill (equal or lower than character level)
+create or replace function skill_min_level_check() returns trigger as $$
+declare
+    n_skill_id n_char_name;
+begin
+-- row will have skill_id and char_name, need to make checks according to that
+n_skill_id = NEW.skill_id;
+n_char_name = NEW.char_name;
+-- need to check if the skill belongs to the charachters class
+if not exists(select * 
+from Earned_Skill ES,  Character CH, Skill S
+-- check if skill_id exists in all skills associated with charachter classname
+-- check if char level is greater than skill level
+where CH.char_name = n_char_name and ES.skill_id = n_skill_id and ES.cls_name = CH.has_class and S.skill_id = ES.skill_id and S.min_level < CH.char_level)
+then 
+raise exception 'Invalid skill assignment, check min level and class for skill';
+end if;
 
-
-
+return NEW;
+end;
+$$ LANGUAGE plpgsql;
 
 -- min level for equipment
 
