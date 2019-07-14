4f0680738ccf059ad19dadf42ff905ed5f9d66ca
Kindeep <kk17xg@brocku.ca>
2019-07-14 02:27:23 -0400

    use DbTest to test database connection

--- src/main/DbTest.java
+++ src/main/DbTest.java
@@ -4,8 +4,16 @@ import org.hibernate.Session;
 
 public class DbTest {
     public static void main(String[] args) {
-        Session session = Database.openSession();
-        session.createQuery("SELECT C FROM GemModel C").list().forEach(System.out::println);
-        session.close();
+        try {
+            Session session = Database.openSession();
+            session.createQuery("SELECT C FROM ClazzModel C").list().forEach(System.out::println);
+            session.close();
+            System.out.println("It is probably working");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Don't think it's working");
+        }
+
     }
 }
