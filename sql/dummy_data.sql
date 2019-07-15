73395334a0f7d45c11c3a02d0b3a6d75e7addae6
Ian Chui <ian.chui@hotmail.com>
2019-07-14 23:01:00 -0400

    fixed a third syntax err.

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -39,14 +39,14 @@ values (0, 'Red', 2, 1, 2, 4, 1, 6, 1, 1),
         (2, 'Blue', 2, 4, 4, 1, 1, 3, 1, 1),
         (3, 'Yellow', 1, 4, 1, 5, 1, 7, 3, 1);
 
-insert into equipment (eqp_name,
+insert into equipment (eqp_name, eqp_id,
                         estrength,
                         edefence,
                         gem_limit)
-values ('Leather Jacket', 1, 2, 3),
-        ('Iron Chainmail', 2, 3, 4),
-        ('Gold Chestplate', 3, 5, 5),
-        ('Diamond Chestplate', 7, 10, 6);
+values ('Leather Jacket', 0, 1, 2, 3),
+        ('Iron Chainmail', 1, 2, 3, 4),
+        ('Gold Chestplate', 2, 3, 5, 5),
+        ('Diamond Chestplate', 3, 7, 10, 6);
 
 insert into armour 
 values (0),
