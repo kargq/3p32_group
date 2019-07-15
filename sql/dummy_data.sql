d275a0b8a17657774ab0ba8fae85870c9142d715
Ian Chui <ian.chui@hotmail.com>
2019-07-14 22:54:06 -0400

    Added gems and armour dummy data.

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -31,8 +31,56 @@ VALUES ('Warrior', 50, 2, 5, 5, 1, 3),
 
 
 /* Test data */
+
+/*---------- gems and armour -------------- */
 INSERT INTO Gem
-values (23412, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+values (0, Red, 2, 1, 2, 4, 1, 6, 1, 1),
+        (1, Green, 9, 4, 2, 5, 1, 3, 6, 1),
+        (2, Blue, 2, 4, 4, 1, 1, 3, 1, 1),
+        (3, Yellow 1, 4, 1, 5, 1, 7, 3, 1);
+
+insert into equipment (eqp_name,
+                        estrength,
+                        edefence
+                        gem_limit)
+values ('Leather Jacket', 1, 2, 3),
+        ('Iron Chainmail', 2, 3, 4),
+        ('Gold Chestplate', 3, 5, 5),
+        ('Diamond Chestplate', 7, 10, 6);
+
+insert into armour 
+values (0),
+        (1),
+        (2),
+        (3);
+
+insert into armour_instance
+values (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 0),
+        (5, 1),
+        (6, 2),
+        (7, 3);
+
+insert into armour_embed
+values (0,4),
+        (1,0),
+        (2,0),
+        (3,3),
+        (4,0),
+        (5,3),
+        (6,0),
+        (7,0),
+        (1,1),
+        (2,1),
+        (3,1),
+        (4,2),
+        (5,2),
+        (6,2),
+        (7,2);
+/*---------- --------------------------- */
 
 insert into character (char_name, has_class)
 values ('Ian', 'Warrior');
