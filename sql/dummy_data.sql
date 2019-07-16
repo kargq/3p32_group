61f4bed1fc314256e8c443609d2b99b7435cc4c0
Kindeep <kk17xg@brocku.ca>
2019-07-16 01:28:04 -0400

    wat ; ;

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -35,72 +35,72 @@ VALUES ('Warrior', 50, 2, 5, 5, 1, 3),
 /*---------- gems and armour -------------- */
 INSERT INTO Gem (gname, glife, gpower, gspeed, gdefence, gwill, gstrength, glevel, gblock)
 values ('Red', 2, 1, 2, 4, 1, 6, 1, 1),
-        ('Green', 9, 4, 2, 5, 1, 3, 6, 1),
-        ('Blue', 2, 4, 4, 1, 1, 3, 1, 1),
-        ('Yellow', 1, 4, 1, 5, 1, 7, 3, 1),
-        ('Black', 2, 4, 2, 5, 2, 2, 6, 2);
+       ('Green', 9, 4, 2, 5, 1, 3, 6, 1),
+       ('Blue', 2, 4, 4, 1, 1, 3, 1, 1),
+       ('Yellow', 1, 4, 1, 5, 1, 7, 3, 1),
+       ('Black', 2, 4, 2, 5, 2, 2, 6, 2);
 
 insert into equipment (eqp_name, estrength, edefence, gem_limit, elevel)
 values ('Leather Jacket', 1, 2, 3, 5),
-        ('Iron Chainmail', 2, 3, 4, 6),
-        ('White t-shirt', 3, 5, 5, 7),
-        ('Black t-shirt', 3, 5, 5, 4),
-        ('Bullet-proof vest', 3, 5, 5, 9),
-        ('Long Sword', 3, 5, 5, 4),
-        ('Staff', 7, 10, 6, 3),
-        ('Lightsaber', 7, 5, 6, 6),
-        ('Whip', 7, 10, 6, 5),
-        ('Brass Knuckles', 7, 5, 6, 4),
-        ('White Gloves', 5, 10, 6, 2),
-        ('Dagger', 2, 2, 2, 2);
+       ('Iron Chainmail', 2, 3, 4, 6),
+       ('White t-shirt', 3, 5, 5, 7),
+       ('Black t-shirt', 3, 5, 5, 4),
+       ('Bullet-proof vest', 3, 5, 5, 9),
+       ('Long Sword', 3, 5, 5, 4),
+       ('Staff', 7, 10, 6, 3),
+       ('Lightsaber', 7, 5, 6, 6),
+       ('Whip', 7, 10, 6, 5),
+       ('Brass Knuckles', 7, 5, 6, 4),
+       ('White Gloves', 5, 10, 6, 2),
+       ('Dagger', 2, 2, 2, 2);
 
 insert into armour (eqp_id)
 values (1),
-        (2),
-        (3),
-        (4),
-        (5);
+       (2),
+       (3),
+       (4),
+       (5);
 
 insert into main_weapon (eqp_id)
-values  (6),
-        (7),
-        (8),
-        (9);
+values (6),
+       (7),
+       (8),
+       (9);
 
 insert into secondary_equipment (eqp_id)
 values (10),
-        (11),
-        (12);
+       (11),
+       (12);
 
 insert into armour_instance (eqp_id)
 values (1),
-        (2),
-        (3),
-        (4),
-        (5),
-        (1),
-        (2),
-        (3),
-        (4),
-        (5);
+       (2),
+       (3),
+       (4),
+       (5),
+       (1),
+       (2),
+       (3),
+       (4),
+       (5);
 
 insert into main_weapon_instance (eqp_id)
 values (6),
-        (7),
-        (8),
-        (9),
-        (6),
-        (7),
-        (8),
-        (9);
+       (7),
+       (8),
+       (9),
+       (6),
+       (7),
+       (8),
+       (9);
 
 insert into secondary_equipment_instance (eqp_id)
 values (10),
-        (11),
-        (12),
-        (10),
-        (11),
-        (12);
+       (11),
+       (12),
+       (10),
+       (11),
+       (12);
 
 insert into armour_embed (armour_instance_id, gem_id)
 values (1, 1),
@@ -272,13 +272,13 @@ insert into clan
 values ('Slayers', 'Sidd');
 
 insert into clan_member
-values ('Slayers','Patrick');
+values ('Slayers', 'Patrick');
 
 insert into clan_member
-values ('Slayers','Sammy');
+values ('Slayers', 'Sammy');
 
 insert into clan_member
-values ('Slayers','Cameron');
+values ('Slayers', 'Cameron');
 
 insert into clan
 values ('Beasties', 'Nate');
@@ -293,7 +293,7 @@ insert into clan_member
 values ('Gwalla', 'Jhonny');
 
 insert into clan_member
-values ('Gwalla','Paul');
+values ('Gwalla', 'Paul');
 
 insert into skill
 values (1, 0, 100, 'Ki blast fired with two hands', 'Kamehameha');
@@ -319,12 +319,12 @@ values (3, 'Black Mage');
 insert into auto_skill
 values (2, 'Ranger');
 
-insert into skill (skill_id ,min_level, power_cost, sname, sdescription)
-values (5, 1, 1, 'Superhuman Strength', 'As a Saiyan, Goku possesses immens'),
-       (6, 2, 2, 'Superhuman Speed', 'Goku can move/react at least as fast a'),
-       (7, 3, 3, 'Superhuman Durabili', 'Goku''s body is far more durable th'),
-       (8, 4, 4, 'Superhuman Senses', 'Goku is able to think and react at ex'),
-       (9, 5, 5, 'Superhuman Enduranc', 'Thanks to his physiology and willpo'),
-       (10, 6, 6, 'Powerful Lungs', 'Thanks to his physiology, Goku possesses'),
-       (11, 7, 7, 'Heat Resistance', 'Thanks to his Saiyan biology, Goku poss');
+insert into skill
+values (5, 1, 1, 'As a Saiyan, Goku possesses immens', 'Superhuman Strength'),
+       (6, 2, 2, 'Goku can move/react at least as fast a', 'Superhuman Speed'),
+       (7, 3, 3, 'Gokus body is far more durable th', 'Superhuman Durabili'),
+       (8, 4, 4, 'Goku is able to think and react at ex', 'Superhuman Senses'),
+       (9, 5, 5, 'Thanks to his physiology and willpo', 'Superhuman Enduranc'),
+       (10, 6, 6, 'Thanks to his physiology, Goku possesses', 'Powerful Lungs'),
+       (11, 7, 7, 'Thanks to his Saiyan biology, Goku poss', 'Heat Resistance');
 
