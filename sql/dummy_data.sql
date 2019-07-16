da73f24a06e5af09aa5e6f4730d8b028bb3ff3f1
cjandress <cameron.andress@hotmail.com>
2019-07-15 20:44:54 -0400

    added dummy data to test auto incrementing

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -40,30 +40,46 @@ values ('Red', 2, 1, 2, 4, 1, 6, 1, 1),
         ('Yellow', 1, 4, 1, 5, 1, 7, 3, 1),
         ('Test', 2, 2, 2, 2, 2, 2, 2, 2);
 
-insert into equipment (eqp_name, eqp_id,
-                        estrength,
-                        edefence,
-                        gem_limit, elevel)
-values ('Leather Jacket', 0, 1, 2, 3, 5),
-        ('Iron Chainmail', 1, 2, 3, 4, 6),
-        ('Gold Chestplate', 2, 3, 5, 5, 4),
-        ('Diamond Chestplate', 3, 7, 10, 6, 5);
+insert into equipment (eqp_name, estrength, edefence, gem_limit, elevel)
+values ('Leather Jacket', 1, 2, 3, 5),
+        ('Iron Chainmail', 2, 3, 4, 6),
+        ('swooords', 3, 5, 5, 4),
+        ('Diamond Chestplate', 7, 10, 6, 5),
+        ('TEST', 2, 2, 2, 2);
 
-insert into armour 
+insert into armour (eqp_id)
 values (0),
         (1),
-        (2),
         (3);
 
-insert into armour_instance (armour_instance_id, eqp_id)
-values (0, 0),
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 0),
-        (5, 1),
-        (6, 2),
-        (7, 3);
+insert into main_weapon (eqp_id)
+values  (2),
+        (2),
+        (1),
+        (3);
+
+insert into secondary_equipment (eqp_id)
+values (4),
+        (1),
+        (3);
+
+insert into armour_instance (eqp_id)
+values (0),
+        (1),
+        (3),
+        (0),
+        (1),
+        (3);
+
+insert into main_weapon_instance (eqp_id)
+values (2),
+        (3),
+        (0);
+
+insert into secondary_equipment_instance (eqp_id)
+values (4),
+        (3),
+        (0);
 
 insert into armour_embed (armour_instance_id, gem_id)
 values (0,0),
